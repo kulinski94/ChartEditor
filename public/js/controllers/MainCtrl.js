5be5c75cb4db9a2103b7a754442fd90069c0fad8
kulinski94 <tsvetelin.kulinski@gmail.com>
2015-07-11 08:28:15 +0300

    asd

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -3,18 +3,33 @@ angular.module('MainCtrl', []).controller('MainController', function ($scope, $h
     console.log("Hello World from controller");
 
     var refresh = function (code) {
-        $http.get("https://demo.trader.bg/charting/rest/candles/EURUSD/ONE_MINUTE?size=10")
+        $http.get("https://demo.trading212.com/charting/rest/candles/EURUSD/ONE_MINUTE?size=5")
     .success(function (data, status, headers, config) {
         console.log("I got the data I requested");
         $scope.candle = { birthTime: "", ask: { open: "", close: "", high: "", low: ""} };
         $scope.candleList = angular.fromJson(data);
-         $scope.code = code;
+        $scope.code = code;
     });
     }
 
     $scope.edit = function (time) {
-        $scope.candle = { birthTime: new Date(time), ask: { open: "", close: "", high: "", low: ""} };;
-    };
+        $http.get("https://demo.trading212.com/charting/rest/candles/EURUSD/ONE_MINUTE")
+    .success(function (data, status, headers, config) {
+        console.log("I got the data I requested");
+        $scope.candle = { birthTime: "", ask: { open: "", close: "", high: "", low: "" }
+        , bid: { open: "", close: "", high: "", low: "" }, volume: ""
+        };
+
+        var list = angular.fromJson(data);
+
+        for (i = 0; i < list.length; i++) {
+            if (list[i].birthTime == time) {
+                $scope.candle = list[i];
+            }
+        }
+
+    });
+    }
 
     $scope.get = refresh;
 
