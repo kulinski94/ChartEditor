eabdfe76f3984fe54448d30ad5af03776d5fa474
kulinski94 <tsvetelin.kulinski@gmail.com>
2015-07-12 22:34:57 +0300

    fix

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,19 +1,14 @@
 // public/js/appRoutes.js
-    angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
     $routeProvider
 
-        // home page
+    // home page
         .when('/', {
             templateUrl: 'views/home.html',
             controller: 'MainController'
-        })
-
-         .when('/candles', {
-            templateUrl: 'views/candles.html',
-            controller: 'NerdController'
         });
 
     $locationProvider.html5Mode(true);
 
-}]);
+} ]);
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -2,11 +2,19 @@
 angular.module('MainCtrl', []).controller('MainController', function ($scope, $http) {
     console.log("Hello World from controller");
 
-    var refresh = function (code) {
-        $http.get("https://demo.trading212.com/charting/rest/candles/EURUSD/ONE_MINUTE?size=5")
+    function init() {
+        $scope.code = "EURUSD";
+
+    }
+    init();
+
+    $scope.get = function (code, from, to) {
+        $http.get("https://demo.trading212.com/charting/rest/candles/" + code + "/ONE_MINUTE?size=5")
     .success(function (data, status, headers, config) {
         console.log("I got the data I requested");
-        $scope.candle = { birthTime: "", ask: { open: "", close: "", high: "", low: ""} };
+        $scope.candle = { birthTime: "", ask: { open: "", close: "", high: "", low: "" }
+        , bid: { open: "", close: "", high: "", low: "" }, volume: ""
+        };
         $scope.candleList = angular.fromJson(data);
         $scope.code = code;
     });
@@ -16,10 +24,6 @@ angular.module('MainCtrl', []).controller('MainController', function ($scope, $h
         $http.get("https://demo.trading212.com/charting/rest/candles/EURUSD/ONE_MINUTE")
     .success(function (data, status, headers, config) {
         console.log("I got the data I requested");
-        $scope.candle = { birthTime: "", ask: { open: "", close: "", high: "", low: "" }
-        , bid: { open: "", close: "", high: "", low: "" }, volume: ""
-        };
-
         var list = angular.fromJson(data);
 
         for (i = 0; i < list.length; i++) {
@@ -31,6 +35,34 @@ angular.module('MainCtrl', []).controller('MainController', function ($scope, $h
     });
     }
 
-    $scope.get = refresh;
 
+    $scope.update = function (candle) {
+        var now = new Date();
+        var timeToChange = new Date(now.getTime() + 3 * 60000);
+        convertToString(candle);
+        $http.post("https://demo.trading212.com/charting/rest/fix", { time: timeToChange.toISOString(), code: $scope.code, candle: candle }).
+  success(function (data, status, headers, config) {
+      // this callback will be called asynchronously
+      // when the response is available
+  }).
+  error(function (data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+  });
+    }
+
+    function convertToString(candle) {
+        candle.ask.open = candle.ask.open.toString();
+        candle.ask.high = candle.ask.high.toString();
+        candle.ask.low = candle.ask.low.toString();
+        candle.ask.close = candle.ask.close.toString();
+
+        
+        candle.bid.open = candle.bid.open.toString();
+        candle.bid.high = candle.bid.high.toString();
+        candle.bid.low = candle.bid.low.toString();
+        candle.bid.close = candle.bid.close.toString();
+
+        candle.birthTime = new Date(candle.birthTime).toISOString();
+    }
 });
