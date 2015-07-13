4c27151d1ac7302ff5ba4ce456404bbf5368a880
kulinski94 <tsvetelin_kulinski@abv.bg>
2015-07-13 18:24:13 +0300

    asdasd

---
+++ config/client.js
@@ -0,0 +1,4 @@
+// config/db.js
+module.exports = {
+    url: 'demo3.testing.trading21.int/charting/rest'
+}
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,102 +1,106 @@
 // public/js/controllers/MainCtrl.js
-angular.module('MainCtrl', ['ui.bootstrap']).controller('MainController', function ($scope, $http, $log) {
+angular.module('MainCtrl', ['ui.bootstrap']).controller('MainController', function ($scope, $http, $log)
+{
 
     console.log("Hello World from controller");
 
-    function init() {
-        $scope.code = "EURUSD";
+    function init()
+    {
+        $scope.code = "BGNPLN";
         $scope.fromDate = new Date();
         $scope.fromTime = new Date();
 
         $scope.toDate = new Date();
         $scope.toTime = new Date();
         $scope.showMeridian = true;
+
+        $scope.hosts = [];
+        $scope.hosts[0] = 'http://demo3.testing.trading212.int:9002/charting/rest/private/fix';
+        //$scope.hosts[1] ="http://feature19.testing.trading212.int:9002/charting/rest/private/fix";
     }
     init();
 
-    $scope.get = function (code, from, to) {
-        $http.get("https://demo.trading212.com/charting/rest/candles/" + code + "/ONE_MINUTE?size=5")
-    .success(function (data, status, headers, config) {
+    $scope.get = function (code, from, to)
+    {
+        $http.get("http://demo3.testing.trading212.int:9002/charting/rest/private/oneMinuteCandles/" + code,
+            { params: { from: $scope.from.getTime(), to: $scope.to.getTime() }
+            })
+    .success(function (data, status, headers, config)
+    {
         console.log("I got the data I requested");
         $scope.candle = { birthTime: "", ask: { open: "", close: "", high: "", low: "" }
         , bid: { open: "", close: "", high: "", low: "" }, volume: ""
         };
         $scope.candleList = angular.fromJson(data);
 
-        for (i = 0; i < $scope.candleList.length; i++) {
+        for (i = 0; i < $scope.candleList.length; i++)
+        {
             $scope.candleList[i].birthTime = new Date($scope.candleList[i].birthTime);
         }
         $scope.code = code;
     });
     }
 
-    $scope.edit = function (time) {
-        for (i = 0; i < $scope.candleList.length; i++) {
-            if ($scope.candleList[i].birthTime == time) {
-                $log.log('edit : ' + $scope.candle);
+    $scope.edit = function (time)
+    {
+        for (i = 0; i < $scope.candleList.length; i++)
+        {
+            if ($scope.candleList[i].birthTime == time)
+            {
+                $log.log('edit : ' + $scope.candleList[i]);
                 $scope.candle = $scope.candleList[i];
             }
         }
     }
 
-
-    $scope.update = function (candle) {
+    $scope.updateResponse = "";
+    $scope.update = function (candle)
+    {
         var now = new Date();
         var timeToChange = new Date(now.getTime() + 3 * 60000);
         var oldCandle = candle;
-        convertToString(candle);
-        $http.post("https://demo.trading212.com/charting/rest/fix", { time: timeToChange.toISOString(), code: $scope.code, candle: candle }).
-  success(function (data, status, headers, config) {
-      // this callback will be called asynchronously
-      // when the response is available
-  }).
-  error(function (data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-  });
-    }
+        candle.birthTime = new Date(candle.birthTime).getTime();
 
-    function formatter(value) {
-        if (angular.isDate(value) && value) {
-            return value.toISOString();
+        $log.log(timeToChange);
+        for (i = 0; i < $scope.hosts.length; i++)
+        {
+            $http.post($scope.hosts[i], { time: timeToChange.getTime(), fixEvent: { periodType: 'ONE_MINUTE', code: $scope.code, candle: candle} }).
+                  success(function (data, status, headers, config)
+                  {
+                      $log.log('success on ' + $scope.hosts[i] + " -> " + angular.fromJson(data));
+                      $scope.updateResponse = angular.fromJson(data);
+                  }).
+                  error(function (data, status, headers, config)
+                  {
+                      //
+                  });
         }
-    }
 
-
-    function convertToString(candle) {
-        candle.ask.open = candle.ask.open.toString();
-        candle.ask.high = candle.ask.high.toString();
-        candle.ask.low = candle.ask.low.toString();
-        candle.ask.close = candle.ask.close.toString();
-
-
-        candle.bid.open = candle.bid.open.toString();
-        candle.bid.high = candle.bid.high.toString();
-        candle.bid.low = candle.bid.low.toString();
-        candle.bid.close = candle.bid.close.toString();
-
-        candle.birthTime = new Date(candle.birthTime).toISOString();
     }
 
     //time picker
 
-    $scope.today = function () {
+    $scope.today = function ()
+    {
         $scope.fromDate = new Date();
         $scope.toDate = new Date();
     };
 
     $scope.today();
     // Disable weekend selection
-    $scope.disabled = function (date, mode) {
+    $scope.disabled = function (date, mode)
+    {
         return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
     };
 
-    $scope.toggleMin = function () {
+    $scope.toggleMin = function ()
+    {
         $scope.minDate = $scope.minDate ? null : new Date();
     };
     $scope.toggleMin();
 
-    $scope.open = function ($event) {
+    $scope.open = function ($event)
+    {
         $event.preventDefault();
         $event.stopPropagation();
 
@@ -127,14 +131,18 @@ angular.module('MainCtrl', ['ui.bootstrap']).controller('MainController', functi
       }
     ];
 
-    $scope.getDayClass = function (date, mode) {
-        if (mode === 'day') {
+    $scope.getDayClass = function (date, mode)
+    {
+        if (mode === 'day')
+        {
             var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
 
-            for (var i = 0; i < $scope.events.length; i++) {
+            for (var i = 0; i < $scope.events.length; i++)
+            {
                 var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
 
-                if (dayToCheck === currentDay) {
+                if (dayToCheck === currentDay)
+                {
                     return $scope.events[i].status;
                 }
             }
@@ -154,39 +162,29 @@ angular.module('MainCtrl', ['ui.bootstrap']).controller('MainController', functi
     };
 
     $scope.ismeridian = false;
-    $scope.toggleMode = function () {
+    $scope.toggleMode = function ()
+    {
         $scope.ismeridian = !$scope.ismeridian;
     };
 
-    $scope.update = function () {
-        var d = new Date();
-        d.setHours(14);
-        d.setMinutes(0);
-        $scope.fromTime = d;
-
-        var d = new Date();
-        d.setHours(14);
-        d.setMinutes(0);
-
-        $scope.toTime = d;
-    };
-
-    $scope.changed = function () {
+    $scope.changed = function ()
+    {
         $scope.from = $scope.fromDate.setHours($scope.fromTime.getHours())
         $scope.from = $scope.fromDate.setMinutes($scope.fromTime.getMinutes())
         $scope.from = $scope.fromDate.setSeconds(0);
-        $scope.from = new Date($scope.from).toISOString();
+        $scope.from = new Date($scope.from);
 
         $scope.to = $scope.toDate.setHours($scope.toTime.getHours())
         $scope.to = $scope.toDate.setMinutes($scope.toTime.getMinutes())
         $scope.to = $scope.toDate.setSeconds(0);
-        $scope.to = new Date($scope.to).toISOString();
+        $scope.to = new Date($scope.to);
 
         $log.log('Time changed to: ' + $scope.from);
     };
     $scope.changed();
 
-    $scope.clear = function () {
+    $scope.clear = function ()
+    {
         $scope.mytime = null;
     };
 });
